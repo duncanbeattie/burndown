0398e81cd29b5af69fd14046ad68d06e9a2eab25
Dan Riti <driti@tracelytics.com>
2013-07-15 22:33:44 +0000

    Add caliper_init helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+    def caliper_init
+        if ENV.has_key?('BURNDOWN_CALIPER_ID')
+            javascript_tag "var Caliper = {config: {apiKey:'%s'}};" % [ENV['BURNDOWN_CALIPER_ID']]
+        end
+    end
+
 end
